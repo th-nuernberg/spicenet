dd7e97b858a33506db1965f737d0f66c504bc13e
stieglerfa84319 <stieglerfa84319@TechnischeHochschuleN.onmicrosoft.com>
2024-06-14 12:01:00 +0200

    first hebbian implementation draft

---
+++ code/embedded/hebbian_correlation_matrix.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from spice_net_som import SpiceNetSom
+
+
+class HebbianCorrelationMatrix:
+
+    def __init__(self, som_1: SpiceNetSom, som_2: SpiceNetSom):
+        self.__weights = np.ones((len(som_1), len(som_2)))
+        self.__activation_bar_1 = np.zeros(len(som_1))
+        self.__activation_bar_2 = np.zeros(len(som_2))
+        self.__som_1 = som_1
+        self.__som_2 = som_2
+
+    def get_matrix(self) -> np.ndarray:
+        return self.__weights
+
+    def fit(self, values_som_1: list[float], values_som_2: list[float], iteration: int):
+        learning_rate_1 = 0.7
+        learning_rate_2 = 0.7
+        k = float(iteration)
+        for i in range(len(values_som_1)):
+            k += 1.0
+            winner_som_1, activation_1 = self.__som_1.get_winning_neuron_index(values_som_1[i])
+            winner_som_2, activation_2 = self.__som_2.get_winning_neuron_index(values_som_2[i])
+
+            fu_1 = (1.0 - learning_rate_2) * self.__activation_bar_1[winner_som_1] * (k - 1.0)
+            fu_2 = learning_rate_2 * activation_1
+            self.__activation_bar_1[winner_som_1] = ((1.0 - learning_rate_2) * self.__activation_bar_1[winner_som_1]
+                                                     * (k - 1.0) + learning_rate_2 * activation_1)
+            self.__activation_bar_2[winner_som_2] = ((1.0 - learning_rate_2) * self.__activation_bar_2[winner_som_2]
+                                                     * (k - 1.0) + learning_rate_2 * activation_2)
+
+            weights_delta = (
+                    learning_rate_1 * (activation_1 - self.__activation_bar_1[winner_som_1])
+                    * (activation_2 - self.__activation_bar_2[winner_som_2])
+            )
+            self.__weights[winner_som_1, winner_som_2] += weights_delta
