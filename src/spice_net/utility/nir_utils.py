3371545636fedef46f0b210f7a94e16101640cc0
Lutz Mitländer <lutzmitlaender@gmx.net>
2025-01-05 22:28:17 +0100

    Add empty planned nir transfer functions

---
+++ src/spice_net/utility/nir_utils.py
@@ -0,0 +1,16 @@
+
+from ..spice_net_som import SpiceNetSom
+from ..spice_net import SpiceNet
+from nir import *
+
+def nir_to_spicenet():
+    pass
+
+def spicenet_to_nir():
+    pass
+
+def som_to_nir():
+    pass
+
+def nir_to_som():
+    pass
