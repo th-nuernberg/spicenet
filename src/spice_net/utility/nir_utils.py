906325870f82cdfb10eee5a4e911ad83107c6660
Lutz Mitländer <lutzmitlaender@gmx.net>
2025-03-20 12:09:24 +0100

    Test clone instructions and add doc string

--- src/spice_net/utility/nir_utils.py
+++ src/spice_net/utility/nir_utils.py
@@ -16,6 +16,20 @@ def from_nir(node: nir.NIRNode,
             som_lrf_interaction_kernel: LearningRateFunction | None = None,
             hcm_lrf_weights: LearningRateFunction | None = None,
             hcm_trust_of_new: LearningRateFunction | None = None) -> SpiceTypes | list[SpiceTypes]:
+    """This function converts NIR nodes back to their SPICEnet representation. This can either be a single SPICEnet component or a whole list of components mapped in a NIR Graph.
+
+    Args:
+        node (nir.NIRNode): NIR node to convert to SPICEnet object
+        som_1 (SpiceNetSom, optional): Only needed if trying to import single HCMs. Defaults to None.
+        som_2 (SpiceNetSom, optional): Only needed if trying to import single HCMs. Defaults to None.
+        som_lrf_tuning_curve (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        som_lrf_interaction_kernel (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_lrf_weights (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_trust_of_new (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+
+    Returns:
+        SpiceTypes | list[SpiceTypes]: Returns a single SPICEnet component or a list of components of the original SPICEnet implementation.
+    """
     # If the node is a graph, we need to iterate over all nodes
     return _from_nir(node, False, som_1, som_2, som_lrf_tuning_curve, som_lrf_interaction_kernel, hcm_lrf_weights, hcm_trust_of_new)
         
@@ -63,6 +77,14 @@ def _map_from_nir(node: nir.NIRNode,
         raise TypeError(f"Unsupported node type {type(node)}")
 
 def to_nir(nodes: SpiceTypes | list[SpiceTypes]) -> nir.NIRNode:
+    """Transforms any SPICEnet component to a NIR node. Can also transform multiple components at once by passing a list of components.
+
+    Args:
+        nodes (SpiceTypes | list[SpiceTypes]): Component or list of components to transform to NIR
+
+    Returns:
+        nir.NIRNode: NIR graph with the components
+    """
     if isinstance(nodes, list):
         return nir.NIRGraph.from_list([_map_to_nir(node) for node in nodes])
     return nir.NIRGraph.from_list([_map_to_nir(nodes)])
@@ -86,7 +108,19 @@ def nir_to_spicenet(
         hcm_lrf_weights: LearningRateFunction | None = None,
         hcm_trust_of_new: LearningRateFunction | None = None
     ) -> SpiceNet | dict[str, SpiceNet]:
-    """Converts a nir spicenet to one or multiple spicenets depending on the number of SOMs."""    
+    """Converts a nir spicenet to one or multiple spicenets depending on the number of SOMs.
+
+    Args:
+        nir_spicenet (nir.SPICENet): object to import
+        som_lrf_tuning_curve (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        som_lrf_interaction_kernel (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_lrf_weights (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_trust_of_new (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+
+    Returns:
+        SpiceNet | dict[str, SpiceNet]: Returns the SPICEnet object or a dictionary of SPICEnet objects if multiple SOMs and HCMs were present in the NIR SPICEnet.
+    """
+    
     # Transform all soms to spicenet soms
     spicenet_soms = {som_name: nir_to_som(nir_som, som_lrf_tuning_curve, som_lrf_interaction_kernel) for som_name, nir_som in nir_spicenet.soms.items()}
     spicenet_values_list = list(spicenet_soms.values())
@@ -111,11 +145,30 @@ def nir_to_spicenet(
     return spicenets
 
 def spicenet_to_nir(spicenet: SpiceNet) -> nir.SPICENet:
-    """Converts a single SPICEnet to a NIR SPICEnet"""
+    """Converts a single SPICEnet to a NIR SPICEnet
+
+    Args:
+        spicenet (SpiceNet): SPICEnet to convert
+
+    Returns:
+        nir.SPICENet: NIR SPICEnet object
+    """
     nir_spicenet = nir.SPICENet.from_lists([som_to_nir(spicenet.som_1), som_to_nir(spicenet.som_2)], [(0, 1, hcm_to_nir(spicenet.get_correlation_matrix()))])
     return nir_spicenet
 
 def nir_to_hcm(nir_spicenet_hcm: nir.SPICEnetHCM, som_1: SpiceNetSom, som_2: SpiceNetSom, hcm_lrf_weights: LearningRateFunction | None = None, hcm_trust_of_new: LearningRateFunction | None = None) -> SpiceNetHcm:
+    """Convert NIR HCM to original SPICEnet HCM.
+
+    Args:
+        nir_spicenet_hcm (nir.SPICEnetHCM): object to convert
+        som_1 (SpiceNetSom): SOM 1 of the HCM.
+        som_2 (SpiceNetSom): SOM 2 of the HCM.
+        hcm_lrf_weights (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_trust_of_new (LearningRateFunction | None, optional): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+
+    Returns:
+        SpiceNetHcm: Original SPICEnet HCM object
+    """
     if hcm_lrf_weights is None:
         if "lrf_weights" in nir_spicenet_hcm.metadata:
             hcm_lrf_weights = get_lr_function_from_metadata(nir_spicenet_hcm.metadata["lrf_weights"])
@@ -137,6 +190,14 @@ def nir_to_hcm(nir_spicenet_hcm: nir.SPICEnetHCM, som_1: SpiceNetSom, som_2: Spi
     return hcm
 
 def hcm_to_nir(spicenet_hcm: SpiceNetHcm) -> nir.SPICEnetHCM:
+    """Transforms a SPICEnet HCM to a NIR SPICEnet HCM.
+
+    Args:
+        spicenet_hcm (SpiceNetHcm): object to transform
+
+    Returns:
+        nir.SPICEnetHCM: NIR SPICEnet HCM object
+    """
     # We need to use a deep copy here. This is so we avoid passing a reference through NIR and then possibly import a reference in another python object in another framework.
     nir_hcm = nir.SPICEnetHCM(weights=spicenet_hcm.weights.copy(),
                               activation_bar_vector_1=spicenet_hcm.activation_bar_vector_1.copy(),
@@ -149,6 +210,14 @@ def hcm_to_nir(spicenet_hcm: SpiceNetHcm) -> nir.SPICEnetHCM:
     return nir_hcm
 
 def som_to_nir(som: SpiceNetSom) -> nir.SPICEnetSOM:
+    """Transforms a SPICEnet SOM to a NIR SPICEnet SOM.
+
+    Args:
+        som (SpiceNetSom): object to transform
+
+    Returns:
+        nir.SPICEnetSOM: NIR SPICEnet SOM object
+    """
     nir_som_neurons = [som_neuron_to_nir(neuron) for neuron in som.neurons]
     return nir.SPICEnetSOM(nir_som_neurons, 
                            metadata={
@@ -165,8 +234,8 @@ def nir_to_som(nir_spicenet_som: nir.SPICEnetSOM, lrf_tuning_curve: LearningRate
 
     Args:
         nir_spicenet_som (nir.SPICEnetSOM): NIR SPICEnetSOM to convert to SpiceNetSom
-        lrf_tuning_curve (LearningRateFunction): Tuning curve learning rate function
-        lrf_interaction_kernel (LearningRateFunction): Interaction kernel learning rate function
+        lrf_tuning_curve (LearningRateFunction | None): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        lrf_interaction_kernel (LearningRateFunction | None): Overwrite learning rate function. Will try to infer if not provided but fail if inference not possible. Defaults to None.
 
     Returns:
         SpiceNetSom: SPICE Net SOM
@@ -193,14 +262,35 @@ def nir_to_som(nir_spicenet_som: nir.SPICEnetSOM, lrf_tuning_curve: LearningRate
     return spicenet_som
 
 def som_neuron_to_nir(spicenet_som_neuron: SpiceNetSom.SomNeuron) -> nir.SPICEnetSOMNeuron:
+    """Transform single SOM neuron to NIR.
+
+    Args:
+        spicenet_som_neuron (SpiceNetSom.SomNeuron): object to transform
+
+    Returns:
+        nir.SPICEnetSOMNeuron: NIR SOM neuron object
+    """
     # creating new reference to avoid changing the original object
     return nir.SPICEnetSOMNeuron(np.array(spicenet_som_neuron.tuning_curve_width), np.array(spicenet_som_neuron.preferred_value))
 
 def nir_to_som_neuron(nir_spicenet_som_neuron: nir.SPICEnetSOMNeuron) -> SpiceNetSom.SomNeuron:
+    """Transform single NIR SOM neuron to SPICEnet SOM neuron.
+
+    Args:
+        nir_spicenet_som_neuron (nir.SPICEnetSOMNeuron): object to transform
+
+    Returns:
+        SpiceNetSom.SomNeuron: original SPICEnet SOM neuron object
+    """
     return SpiceNetSom.SomNeuron(nir_spicenet_som_neuron.mean.item(), nir_spicenet_som_neuron.std.item())
 
 def export_to_hdf5(filepath: str , nodes: SpiceTypes | list[SpiceTypes]):
-    """Exports a SPICEnet to a HDF5 file."""
+    """Exports SPICEnet compontent(s) to a HDF5 file.
+
+    Args:
+        filepath (str): Path for file to export to
+        nodes (SpiceTypes | list[SpiceTypes]): Object or list of objects to export
+    """
     
     nir_graph = to_nir(nodes)
     nir.write(filepath, nir_graph)
@@ -213,8 +303,21 @@ def read_from_hdf5(
         som_lrf_interaction_kernel: LearningRateFunction = None,
         hcm_lrf_weights: LearningRateFunction = None,
         hcm_trust_of_new: LearningRateFunction = None
-    ) -> SpiceNet:
-    """Reads a SPICEnet from a HDF5 file."""
+    ) -> SpiceTypes | list[SpiceTypes]:
+    """This function converts NIR nodes in HDF6 format back to their SPICEnet representation. This can either be a single SPICEnet component or a whole list of components mapped in a NIR Graph.
+
+    Args:
+        filepath (str): HDF5 path to read from
+        som_1 (SpiceNetSom, optional): Only needed if trying to import single HCMs. Defaults to None.
+        som_2 (SpiceNetSom, optional): Only needed if trying to import single HCMs. Defaults to None.
+        som_lrf_tuning_curve (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        som_lrf_interaction_kernel (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_lrf_weights (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+        hcm_trust_of_new (LearningRateFunction | None, optional): Overwrite learning rate function on import. Will try to infer if not provided but fail if inference not possible. Defaults to None.
+
+    Returns:
+        SpiceTypes | list[SpiceTypes]: Returns a single SPICEnet component or a list of components of the original SPICEnet implementation.
+    """
     
     nir_spicenet = nir.read(filepath)
     return from_nir(nir_spicenet, som_1, som_2, som_lrf_tuning_curve, som_lrf_interaction_kernel, hcm_lrf_weights, hcm_trust_of_new)
